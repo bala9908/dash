e4f2e68227fd1d8b666ac3dbb390cae3a2a027f8
unknown <chandrabala547@gmail.com>
2025-05-26 14:43:32 +0530

    images updated-2

--- src/data.js
+++ src/data.js
@@ -1,30 +1,30 @@
  let data=[
     {
-        image1:'../src/images/Heart.png'
+        image1:'../public/images/Heart.png'
 
     },
     {
-        image2:'../src/images/Leg.png' //bones
+        image2:'../public/images/Leg.png' //bones
     },
     {
-        image3:'../src/images/Lungs.png'
+        image3:'../public/images/Lungs.png'
     },
     {
-        image4:'../src/images/Teeth.png'
+        image4:'../public/images/Teeth.png'
     },
     {
-        image5:'../src/images/Bones.png'
+        image5:'../public/images/Bones.png'
     },{
-        image6:'../src/images/Strong.png'
+        image6:'../public/images/Strong.png'
     },
     {
-        image7:'../src/images/green-syringe-element-vector_53876-140866-removebg-preview.png'
+        image7:'../public/images/green-syringe-element-vector_53876-140866-removebg-preview.png'
     },
     {
-        Image8:'../src/images/Eye.png'
+        Image8:'../public/images/Eye.png'
     },
     {
-        Image9:'../src/images/doctor-icon-flat-art-black-white-isolated_1223784-16889-removebg-preview.png'
+        Image9:'../public/images/doctor-icon-flat-art-black-white-isolated_1223784-16889-removebg-preview.png'
     }
 
 ]
